e7ea6b2f4b5b798196282b6ede38b8e44370c0ad
Jómagam <x@x-VirtualBox>
2016-11-29 10:59:36 +0100

    Deleted unrelated stuff

--- real-estate/real-estate-dao/src/main/java/hu/iit/uni/miskolc/iit/webtech/real/estate/dao/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * This package contains the interfaces and exceptions related to data access and storage.
- */
-package hu.iit.uni.miskolc.iit.webtech.real.estate.dao;
-
--- real-estate/real-estate-model/src/main/java/hu/iit/uni/miskolc/iit/webtech/real/estate/model/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Domain class used by the real-estate example.
- */
-package hu.iit.uni.miskolc.iit.webtech.real.estate.model;
-
